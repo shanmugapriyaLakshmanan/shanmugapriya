44d635be08a246c45d132daa7d1c2a8eb09ffa00
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-07-22 10:59:15 +0530

    Create Nnumber.java

---
+++ Nnumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Nnumber {
+	public static void main(String[] args) {
+		int a;
+		String s;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		s=in.next();
+		for(int i=1;i<=a;i++){
+			System.out.println(s);
+		}
+	}
+}

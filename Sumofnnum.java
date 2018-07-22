73ab6064f6870c9957fd4e6b70609d8e33ac8379
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-07-22 11:37:37 +0530

    Create Sumofnnum.java

---
+++ Sumofnnum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Sumofnnum {
+	public static void main(String[] args) {
+		int N=0,K,sum=0;
+		Scanner in=new Scanner(System.in);
+		N=in.nextInt();
+		int a[]=new int[N];
+		for(int i=0;i<N;i++){
+			a[i]=in.nextInt();
+		}
+		K=in.nextInt();
+        for(int i=0;i<K;i++){
+        	sum=sum+a[i];
+        }
+		System.out.println(sum);
+	}
+}

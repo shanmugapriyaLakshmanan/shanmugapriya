4726a28833e774fde5ea566340f0534e603de60e
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-07-22 10:09:55 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class Vowel {
-
 	public static void main(String[] args) {
 		char ch;
 		Scanner in=new Scanner(System.in);
@@ -8,9 +7,11 @@ public class Vowel {
 		if(ch=='a' || ch=='e'|| ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
 			System.out.println("the given character is a vowel");
 		}
+		if(ch=='!'||ch=='@'||ch=='#'||ch=='$'||ch=='%'||ch=='^'||ch=='&'||ch=='*'||ch=='('||ch==')'){
+			System.out.println("invalid character");
+		}	
 		else{
 			System.out.println("the given character is consonant");
 		}
 	}
-
 }

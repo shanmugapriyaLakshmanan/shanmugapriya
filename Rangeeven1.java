10b3464889641789f79710d87a0cdcb6ac1c1011
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-07-23 18:07:11 +0530

    Update and rename Rangeeven.java to Rangeeven1.java

--- Rangeeven1.java
+++ Rangeeven1.java
@@ -1,12 +1,12 @@
 import java.util.*;
-public class Rangeeven {
+public class Rangeeven1 {
   public static void main(String[] argv) {
      int N,Q;
       Scanner in=new Scanner(System.in);
       N=in.nextInt();
       Q=in.nextInt();
       if(Q>0 && Q<=100000){
-    	  for(int i=N;i<Q;i++){
+    	  for(int i=N+1;i<Q;i++){
     		  if(i%2==0){
     			  System.out.print(i+" ");
     		  }

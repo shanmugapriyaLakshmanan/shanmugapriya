49f46ae7456fda2987f8cb8c0311f2af484084ba
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-07-24 13:53:57 +0530

    Create Amstrong1.java

---
+++ Amstrong1.java
@@ -0,0 +1,28 @@
+package digi;
+import java.util.*;
+public class Amstrong1 {
+  public static void main(String[] argv) {
+	  int N,a=0,b,d=0,x,count=0,e=0;
+      Scanner in=new Scanner(System.in);
+      N=in.nextInt();
+         a=N;
+     while(a!=0){
+    	 a=a/10;
+    	 count++;
+        }
+     e=N;
+      while(e!=0){
+    	 b=e%10;
+    	int c=(int)Math.pow(b,count);
+    	d=d+c;
+    	 e=e/10;
+       }
+     if(d==N){
+    	 System.out.println("yes");  
+        }
+     else{
+    	 System.out.println("no");
+     }
+      }
+  
+}

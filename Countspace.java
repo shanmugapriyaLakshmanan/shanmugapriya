cafa7bb41fc83dcaba0933bfa05ea957c3316ad5
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-08 15:15:13 +0530

    Create Countspace.java

---
+++ Countspace.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Countspace {
+	public static void main(String[] args) {
+     Scanner in=new Scanner(System.in);
+     String s=in.nextLine();
+     String s1[]=s.split(" ");
+     System.out.println(s1.length-1);
+	}
+}

933ff60fcea066890f5011b3f22aaab4e564e89c
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-15 14:52:01 +0530

    Create S41.java

---
+++ S41.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class S41{
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String s=in.next();
+	int n=in.nextInt();
+	for(int i=0;i<n;i++){
+		System.out.println(s);
+	}
+	}
+}

ab654cf1279d637ec314d193be448f998a7b322e
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-19 10:40:17 +0530

    Create Set49.java

---
+++ Set49.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Set49 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int a=in.nextInt();
+		System.out.println(n+a);
+	}
+}

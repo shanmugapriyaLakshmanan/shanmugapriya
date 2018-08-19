b38ded421f0e91a3ef1b79b3e571286b6e506556
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-19 10:06:41 +0530

    Create Set46.java

---
+++ Set46.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Set46 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+			System.out.println(n+1);
+	}
+}

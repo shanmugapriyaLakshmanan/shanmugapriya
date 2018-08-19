abb22aaaad8e21f3a6c430f22b9373f7e2525a7d
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-19 10:05:57 +0530

    Create Set45.java

---
+++ Set45.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Set45 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int l=Integer.valueOf(String.valueOf(n).length());
+			System.out.println(l);
+	}
+}

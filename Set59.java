fdaa2b01118686737ad2315b3ca342577191198d
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-19 14:15:43 +0530

    Create Set59.java

---
+++ Set59.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class Set59{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int count=Integer.valueOf(String.valueOf(n).length());
+		System.out.println(count);
+	}
+}

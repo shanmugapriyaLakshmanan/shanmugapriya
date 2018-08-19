18a358be161916ea239778bd6ef1e922782f77f7
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-19 10:05:03 +0530

    Create Set44.java

---
+++ Set44.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Set44 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		if(n>1 && n<10)
+		System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+}

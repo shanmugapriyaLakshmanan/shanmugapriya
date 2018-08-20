faba0fbb02fe9acbd545a3a64e83dfb7456db818
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-20 09:23:13 +0530

    Create Set70.java

---
+++ Set70.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Set70 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int a=2*n;
+		System.out.println(a);
+	}
+}

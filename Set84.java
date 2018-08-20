5b3e0a7f3792bdca2ab95f1d09c25f477345f23a
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-20 21:00:02 +0530

    Update Set84.java

--- Set84.java
+++ Set84.java
@@ -3,7 +3,7 @@ public class Set84 {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		char ch=in.next().charAt(0);
-		if((ch>=65 && ch<=90)||(ch>=97 && ch<=122))
+		if((ch>=65 && ch<=90)||(ch>=97 && ch<=122)||(ch>=48 && ch<=57))
 			System.out.println((int)ch);
 	}
 }

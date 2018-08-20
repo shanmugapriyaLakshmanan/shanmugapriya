861c7c83280bc8546946e50a2fe9a3480a5a5f6c
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-20 11:44:13 +0530

    Create Set78.java

---
+++ Set78.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Set78 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+			if(n%13==0)
+				System.out.print("yes");
+			else
+				System.out.println("no");
+		}
+	}

2d740511e99e11638c1c39af9a455ef36c7ae611
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-20 09:19:30 +0530

    Create Set61.java

---
+++ Set61.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Set61 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String s=in.nextLine();
+	     String s1="";
+		int k=in.nextInt();
+		s1=s.substring(0,k);
+		System.out.println(s1);
+	}
+}

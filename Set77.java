f8c49d483fd49ba4b68990e579d3bc5f3994b76a
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-20 11:42:08 +0530

    Create Set77.java

---
+++ Set77.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Set77 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		for(int i=1;i<=n;i++){
+			if(n%i==0)
+				System.out.print(i+" ");
+		}
+	}
+}

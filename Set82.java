2d035f05b70cb5e19c4444c495875263748a4a51
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-20 19:10:17 +0530

    Create Set82.java

---
+++ Set82.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Set82{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		float a=in.nextFloat();
+		float b=in.nextFloat();
+	    float area;
+	    area=(a*b);
+	    System.out.printf("%.5f",area);
+	}
+}

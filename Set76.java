80880c4ee283af6b2e26a78df44ac7b2714eeb95
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-20 11:41:22 +0530

    Create Set76.java

---
+++ Set76.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Set76 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int count=0;
+		for(int i=2;i<=n;i++){
+			if(n%i==0)
+				count++;	
+		}
+		if(count==1)
+			System.out.println("no");
+		else
+			System.out.println("yes");
+	}
+}

ccdcab0b02045ab2d5ca01946f0eb2b5e4b8130c
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-20 09:22:28 +0530

    Create Set68.java

---
+++ Set68.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Set68 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		if(n%7==0)
+			System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+}

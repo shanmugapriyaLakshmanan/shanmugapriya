2f25e6cc98a7236b8006dfc7b6ecb98bbd10c526
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-21 15:38:18 +0530

    Create Set96.java

---
+++ Set96.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Set96 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();int c=in.nextInt();
+		int ap=(c/2)*((2*a)+(c-1)*b);
+		//System.out.println((n/2)*((2*a)+(n-1)*d));
+		System.out.println(ap);
+	}
+}

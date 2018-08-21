7bd5eee03d6e495bc3d3da1f0effae1f48af317e
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-21 18:21:42 +0530

    Create Set104.java

---
+++ Set104.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Set104 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int k=in.nextInt();
+		int a=(int)Math.pow(n,k);
+		System.out.println(a);
+	}
+}

03cd1174d05e21f86561914fec47af8e68b2996f
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-21 18:23:16 +0530

    Create Set107.java

---
+++ Set107.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Set107 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int c=in.nextInt();
+		int s=(a*b)/c;
+		System.out.println(s);
+		}
+}

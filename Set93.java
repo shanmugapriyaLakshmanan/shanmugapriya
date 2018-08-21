886447be14e17334c7d914fca457066aa2487c14
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-21 15:36:48 +0530

    Create Set93.java

---
+++ Set93.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Set93 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter a temperature in celsius");
+		int n=in.nextInt();
+		System.out.println("the temperature in kelvin is"+(n+273));
+	}
+}

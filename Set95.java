70cae32f3a1446ba3908be8da1cbaabd52efcad1
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-21 15:37:54 +0530

    Create Set95.java

---
+++ Set95.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Set95 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int p=in.nextInt();
+		int n=in.nextInt();
+		int t=in.nextInt();
+		int si=(p*n*t)/100;
+		System.out.println(si);
+	}
+}

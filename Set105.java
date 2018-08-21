f20e96440f7a92b61b32df17e20aa38f63975581
shanmugapriyaLakshmanan <41136410+shanmugapriyaLakshmanan@users.noreply.github.com>
2018-08-21 18:22:13 +0530

    Create Set105.java

---
+++ Set105.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Set105 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int k=in.nextInt();
+		String s=String.valueOf(n)+String.valueOf(k);
+		System.out.println(Integer.valueOf(s));
+	}
+}
